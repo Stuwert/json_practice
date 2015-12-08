9d2b36272926ead14225a0a8d13cddb379fa3db3
Stuart Urback <stuart@urback.net>
2015-12-08 11:55:59 -0700

    Complete Sum All Functionality

--- thing.js
+++ thing.js
@@ -18,10 +18,10 @@ function filterOnSearch(m){
 function printToH2(m){
   var newData = m.data;
   var newDiv = $("<div></div>").appendTo('body');
-  $("<a href='http://www.reddit.com" + newData['url']+"'>"+"<h2>" + newData["title"] + "</h2>" + "</a>").appendTo(newDiv);
+  $("<a href='" + newData['url']+"'>"+"<h2>" + newData["title"] + "</h2>" + "</a>").appendTo(newDiv);
   $()
-  if (newData["header_img"] !== null){
-    newDiv.append("<img src='" + newData["header_img"] + "' />");
+  if ("preview" in newData){
+    newDiv.append("<img src='" + newData["preview"]["images"][0]["source"]["url"] + "' width='150' height='150' />");
   }else{
     newDiv.append("<img src='" + "http://placehold.it/150x150" + "' />");
   }
@@ -37,14 +37,13 @@ $('#search').click(function(){
   var searchValue = $('#searchInput').val();
   $('div').remove();
   var getter = $.ajax({
-    url:"https://www.reddit.com/subreddits/search.json?q=" + searchValue,
+    url:"https://www.reddit.com/search.json?q=" + searchValue,
     method: "GET",
     dataType: "json"
   });
 
   getter.done(function(response){
     console.log("we have the cookie!");
-    var response = getter.responseJSON;
     dataIWant = response["data"]["children"];
     init();
     filter();
@@ -60,8 +59,6 @@ function filter(){
   var filterValue = $('#filterInput');
   $('div').remove();
   var newArray = dataIWant.filter(filterOnSearch, filterValue);
-  for (var i=0; i<newArray.length; i++){
-  }
   newArray.forEach(printToH2);
 }
 
